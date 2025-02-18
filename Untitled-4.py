e845586afc556fb8fb7a435bc55aa92c308ad0e0
Warkoz06 <haozhu@alu.edu.gva.es>
2025-02-18 12:12:05 +0100

    Add files via upload

---
+++ Untitled-4.py
@@ -0,0 +1,13 @@
+"""Ejercicio 1"""
+a = float(input("introduce el valor del lado de un cuadrado en metros: "))
+print(f"El área del cuadrado es {round(a*a,2)} metros^2 y el perímetro es {a*4} metros")
+
+"""Ejercicio 2"""
+b = float(input("Introduce el lado 1 del rectángulo en metros: "))
+c = float(input("Introduce el lado 2 del rectánguloen metros: "))
+print(f"El área del rectángulo es {b*c} metros^2 y el perímetro es {b*2+c*2} metros")
+
+"""Ejercicio 3 """
+d = float(input("Introduce el valor de la base del triángulo en metros: "))
+e = float(input("Introduce el valor de la altura del triángulo en metros: "))
+print(f"El área del triángulo es {(d*e)/2} metros")
